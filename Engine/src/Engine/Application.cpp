18bda87a61b078e89ce1c8a899d7e7623d577e8c
Jay Sharma <j.sharma@digipen.edu>
2020-08-14 17:59:14 -0700

    Event System Completed

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include "Engine/Events/ApplicationEvent.h"
+#include "Engine/Log.h"
 
 namespace Engine
 {
@@ -12,6 +14,16 @@ namespace Engine
 
   void Application::Run()
   {
+    WindowResizeEvent e(1920, 1080);
+    if (e.IsInCategory(EventCategoryApplication))
+    {
+      EN_TRACE(e);
+    }
+    if (e.IsInCategory(EventCategoryInput))
+    {
+      EN_TRACE(e);
+    }
+
     while (true);
   }
 }
